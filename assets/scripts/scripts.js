7efa1f12e22e5825cd9bc1bcbf5db1004343293f
fkakatie <ksexton@adobe.com>
2024-01-21 10:24:59 -0700

    feat: enable week buttons

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,4 +1,4 @@
-const APP_ID = 'AKfycbx2kp7LH_fXKGudXNjoqqGZi4cAV0xoxM9nu81paOKXMOFuCYBrMYKxYQq57D-wnj_J';
+const APP_ID = 'AKfycbzU62vPsIIWig3QD8sbAsid6YUiAhvC7Yh2w8fhYpNJZF45j3j-z2ck6h03lOXE1jrf';
 const APP_URL = `https://script.google.com/macros/s/${APP_ID}/exec`;
 
 // utilities
@@ -50,11 +50,11 @@ async function fetchFragment(fragmentName) {
   try {
     const resp = await fetch(`fragments/${fragmentName}.html`);
     const fragmentText = await resp.text();
-    const fragment = createEl('div', {
-      html: fragmentText,
-      class: 'fragment',
-    });
-    fragment.dataset.name = fragmentName;
+    let fragment = createEl('div', { html: fragmentText });
+    // check if fragment is self-contained in a wrapping element
+    const wrapped = fragment.firstElementChild === fragment.lastElementChild;
+    if (wrapped) fragment = fragment.firstElementChild;
+    fragment.dataset.source = 'fragment';
     swapIcons(fragment);
     return fragment;
   } catch (error) {
@@ -83,14 +83,27 @@ async function gapi(params, key = auth(), method = 'GET') {
 
 async function fetchTasks() {
   if (!window.tasks) {
-    const tasks = [];
+    const tasks = {};
     const habits = await gapi({ sheet: 'habit' });
-    if (habits.data) tasks.push(...habits.data);
+    if (habits.data) {
+      // eslint-disable-next-line no-return-assign
+      habits.data.forEach((habit) => tasks[habit.id] = habit);
+    }
     const chores = await gapi({ sheet: 'chores' });
-    if (chores.data) tasks.push(...chores.data);
+    if (chores.data) {
+      // eslint-disable-next-line no-return-assign
+      chores.data.forEach((chore) => tasks[chore.id] = chore);
+    }
     window.tasks = tasks;
   }
   return window.tasks;
+}
+
+function returnYMD(date) {
+  const yy = date.getFullYear().toString().slice(2);
+  const mm = date.getMonth().toString().padStart(2, '0');
+  const dd = date.getDate().toString().padStart(2, '0');
+  return { yy, mm, dd };
 }
 
 async function fetchCompletedTasks() {
@@ -98,9 +111,7 @@ async function fetchCompletedTasks() {
   const log = {};
   tracker.data.forEach((row) => {
     const timestamp = new Date(row.timestamp);
-    const yy = timestamp.getFullYear().toString().slice(2);
-    const mm = timestamp.getMonth().toString().padStart(2, '0');
-    const dd = timestamp.getDate().toString().padStart(2, '0');
+    const { yy, mm, dd } = returnYMD(timestamp);
     const monthKey = `m${mm}`;
     const dateKey = `d${mm}${dd}${yy}`;
     // check if month
@@ -123,9 +134,7 @@ async function fetchCompletedTasks() {
 
 async function filterCompletedTasks(date = new Date()) {
   const timestamp = date;
-  const yy = timestamp.getFullYear().toString().slice(2);
-  const mm = timestamp.getMonth().toString().padStart(2, '0');
-  const dd = timestamp.getDate().toString().padStart(2, '0');
+  const { yy, mm, dd } = returnYMD(timestamp);
   const monthKey = `m${mm}`;
   const log = await fetchCompletedTasks();
   if (log[monthKey]) {
@@ -138,21 +147,20 @@ async function filterCompletedTasks(date = new Date()) {
 
 async function completeTask(id, frequency, time = '') {
   const resp = await gapi({ id, frequency, time }, auth(), 'POST');
-  console.log(resp);
+  console.log(resp.id, resp.message);
 }
 
 // dom builders
 function buildTask(el, task) {
-  const thisTask = el.querySelector('.task');
   // populate body
-  const icon = thisTask.querySelector('.task-icon');
+  const icon = el.querySelector('.task-icon');
   icon.textContent = task.icon;
-  const eyebrow = thisTask.querySelector('.task-eyebrow');
+  const eyebrow = el.querySelector('.task-eyebrow');
   eyebrow.textContent = `${task.room} • ~${task.mins} minutes`;
-  const title = thisTask.querySelector('.task-title');
+  const title = el.querySelector('.task-title');
   title.textContent = task.task;
   // populate form
-  const form = thisTask.querySelector('.task-form');
+  const form = el.querySelector('.task-form');
   const label = form.querySelector('label');
   label.setAttribute('for', task.id);
   label.setAttribute('aria-label', `mark ${task.task} complete`);
@@ -163,21 +171,48 @@ function buildTask(el, task) {
   input.addEventListener('change', () => {
     if (input.checked) {
       checkbox.dataset.animate = true;
-      thisTask.dataset.animate = true;
+      // eslint-disable-next-line no-param-reassign
+      el.dataset.animate = true;
       input.disabled = true;
       // update spreadsheet
       console.log(`TASK ${task.id}:`, task.task);
       completeTask(task.id, task.freq);
     } else {
       checkbox.removeAttribute('data-animate');
-      thisTask.removeAttribute('data-animate');
+      el.removeAttribute('data-animate');
     }
   });
   // add attributes
   Object.keys(task).forEach((key) => {
-    if (task[key] !== '') thisTask.dataset[key] = task[key];
+    // eslint-disable-next-line no-param-reassign
+    if (task[key] !== '') el.dataset[key] = task[key];
   });
-  return thisTask;
+  return el;
+}
+
+async function refreshTasks(day, date, index) {
+  const todayIndex = parseInt(document.querySelector('.week').parentElement.dataset.today, 10);
+  const future = index > todayIndex;
+  // clear all tasks
+  day.querySelectorAll('.task[data-id]').forEach((task) => {
+    const cb = task.querySelector('input');
+    // eslint-disable-next-line no-param-reassign
+    cb.disabled = false;
+    // eslint-disable-next-line no-param-reassign
+    cb.checked = false;
+    // eslint-disable-next-line no-param-reassign
+    cb.disabled = future; // disable selecting tasks for future dates
+  });
+  // mark completed tasks
+  const filtered = await filterCompletedTasks(date);
+  filtered.forEach((task) => {
+    const checkbox = day.querySelector(`.task[data-id="${task.taskId}"] input`);
+    if (checkbox) {
+      // this will NOT trigger the 'change' event
+      checkbox.checked = true;
+      checkbox.disabled = true;
+    }
+  });
 }
 
 async function buildWeek() {
@@ -191,19 +226,40 @@ async function buildWeek() {
     date.setDate(date.getDate() - 1);
     str = date.toString().toLowerCase();
   }
-  for (let i = 0; i < days.length; i += 1) {
-    const day = days[i];
+  let todayIndex = 0;
+  days.forEach((day, i) => {
     const dateStr = date.toString().toLowerCase();
     if (today.slice(0, 15) === dateStr.slice(0, 15)) {
+      todayIndex = i;
       day.setAttribute('aria-selected', true);
     }
+    const { yy, mm, dd } = returnYMD(date);
+    // eslint-disable-next-line no-param-reassign
+    day.dataset.mkey = `m${mm}`;
+    // eslint-disable-next-line no-param-reassign
+    day.dataset.dkey = `d${mm}${dd}${yy}`;
+    // eslint-disable-next-line no-param-reassign
+    day.dataset.index = i;
     const text = day.querySelector('.day-text');
     text.textContent = dateStr.slice(0, 2);
     const num = day.querySelector('.day-num');
     num.textContent = date.getDate();
+    day.addEventListener('click', () => {
+      // select button
+      days.forEach((d) => d.setAttribute('aria-selected', false));
+      day.setAttribute('aria-selected', true);
+      // reset day with completed tasks
+      const [m, d, y] = day.dataset.dkey.replace('d', '').match(/.{1,2}/g).map((char) => parseInt(char, 10));
+      refreshTasks(
+        document.querySelector('.day'), // day el containing tasks
+        new Date(`${m + 1}/${d}/${y}`), // date to filter by
+        parseInt(day.dataset.index, 10), // index of date to filter by
+      );
+    });
     // set date to next day
     date.setDate(date.getDate() + 1);
-  }
+  });
+  week.dataset.today = todayIndex;
   return week;
 }
 
@@ -224,7 +280,7 @@ async function buildDashboard(main) {
   const taskTemplate = await fetchFragment('task');
   const buildDummyTask = () => {
     const task = taskTemplate.cloneNode(true);
-    task.querySelector('.task').classList.add('task-dummy');
+    task.classList.add('task-dummy');
     task.querySelector('input').name = 'dummy';
     day.append(task);
   };
@@ -235,14 +291,12 @@ async function buildDashboard(main) {
   setTimeout(() => clearInterval(timer));
   const dummies = day.querySelectorAll('.task-dummy');
   // add tasks
-  tasks.forEach((task, i) => {
-    const el = buildTask(taskTemplate.cloneNode(true), task);
-    if (dummies[i]) dummies[i].parentElement.replaceWith(el);
+  Object.keys(tasks).forEach((id, i) => {
+    const el = buildTask(taskTemplate.cloneNode(true), tasks[id]);
+    if (dummies[i]) dummies[i].replaceWith(el);
     else day.append(el);
   });
-  dummies.forEach((dummy) => {
-    if (dummy.parentElement) dummy.parentElement.remove();
-  });
+  dummies.forEach((dummy) => dummy.remove());
 
   // mark completed tasks
   const filtered = await filterCompletedTasks();
@@ -320,6 +374,7 @@ async function buildLogin(main) {
 
 function init() {
   swapIcons(document.body);
+  document.body.dataset.status = 'loaded';
 
   const authenticated = auth();
   const main = document.querySelector('main');
